707e9f5f0487907b537059bdf7f4f8fe1ac418de
Katja <47885564+katjalennartz@users.noreply.github.com>
2021-09-10 19:04:17 +0200

    add delete for modnotifications

--- upload/inc/tasks/reservations.php
+++ upload/inc/tasks/reservations.php
@@ -24,7 +24,21 @@ function task_reservations($task)
   while ($entry = $db->fetch_array($get_type)) {
     // $db->write_query("SELECT * FROM mybb_reservationsentry WHERE type = '{$entry['type']}' and DATE_ADD(enddate, INTERVAL {$entry['member_lock']} DAY) < CURDATE()");
     $db->delete_query("reservationsentry", "type = '{$entry['type']}' and DATE_ADD(enddate, INTERVAL {$entry['member_lock']} DAY) < CURDATE()");
+    $db->delete_query("reservationsmodread", "entry_id NOT IN (SELECT entry_id FROM ".TABLE_PREFIX."reservationsentry");
+
+    // SELECT * FROM `mybb_reservationsmodread` WHERE entry_id NOT IN (SELECT entry_id FROM mybb_reservationsentry)
   }
   add_task_log($task, "Reservierungen bereinigt");
 
 }
+
+
+// global $db, $mybb, $lang;
+// $get_type = $db->simple_select("reservationstype", "*");
+// while ($entry = $db->fetch_array($get_type)) {
+//   echo "test".$entry['member_lock'];
+//   $db->delete_query("reservationsentry", "type = '{$entry['type']}' and DATE_ADD(enddate, INTERVAL {$entry['member_lock']} DAY) < CURDATE()");
+//  }
+
+// SELECT entry_id, startdate, enddate, DATE_ADD(enddate, INTERVAL 7 DAY)as test FROM mybb_reservationsentry WHERE DATE_ADD(enddate, INTERVAL 7 DAY) < CURDATE()
+// }
