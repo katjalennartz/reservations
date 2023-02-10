0e439c2dfcacc433151d0e359fa1643a9839f82d
Katja <47885564+katjalennartz@users.noreply.github.com>
2023-02-10 10:46:40 +0100

    Add files via upload

--- upload/inc/languages/deutsch_du/reservations.lang.php
+++ upload/inc/languages/deutsch_du/reservations.lang.php
@@ -5,5 +5,21 @@
 
 $l['reservations'] = 'Reservierungen';
 
+$l['reservations_delete'] = '[x]';
+$l['reservations_edit'] = '[e]';
+$l['reservations_extend'] = '[+]';
+$l['reservations_hide'] = '[hide]';
 
-?>
+$l['reservations_delete_confirm'] = 'Möchtest du den Eintrag wirklich löschen?';
+$l['reservations_extend_confirm'] = 'Möchtest du den Eintrag verlängern?';
+$l['reservations_hide_confirm'] = 'Möchtest du den Eintrag wirklich verbergen?';
+
+$l['reservations_extend_no'] = '(verlängern nicht mehr möglich)';
+$l['reservations_markall'] = '[mark all]';
+
+//Errors
+$l['reservations_entry_exists'] = "Es gibt schon eine Reservierung mit diesen Eintrag.";
+$l['reservations_entry_exists_member'] = "Es gibt schon einen solchen Eintrag von einem der Mitglieder.";
+$l['reservations_entry_exists_duration'] = "Du hast diese Reservierung schon einmal getätigt und der Zeitraum bis du sie erneut vornehmen kannst ist noch nicht verstrichen.";
+
+
