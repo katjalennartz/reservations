71e84688604a68e4f08ab1ae7144a04b23f07814
Katja <47885564+katjalennartz@users.noreply.github.com>
2023-04-15 11:29:17 +0200

    bugfix: syntax error

--- upload/inc/tasks/reservations.php
+++ upload/inc/tasks/reservations.php
@@ -14,15 +14,20 @@
 function task_reservations($task)
 {
   global $db, $mybb, $lang;
+
   //alle typen bekommen
   $get_type = $db->simple_select("reservationstype", "*");
   //für jeden typen nacheinander schauen
   while ($entry = $db->fetch_array($get_type)) {
+    //es gibt eine sperrfrist, entsprechend muss der eintrag gespeichert werden. 
     if ($entry['member_lock'] != 0) {
-    //jetzt die abgelaufenen löschen
+         //jetzt die abgelaufenen löschen
       $db->delete_query("reservationsentry", "type = '{$entry['type']}' and DATE_ADD(enddate, INTERVAL {$entry['member_lock']} DAY) < CURDATE()");
-      $db->delete_query("reservationsmodread", "entry_id NOT IN (SELECT entry_id FROM " . TABLE_PREFIX . "reservationsentry");
+      $db->delete_query("reservationsmodread", "entry_id NOT IN (SELECT entry_id FROM " . TABLE_PREFIX . "reservationsentry)");
+    } else {
+      $db->delete_query("reservationsentry", "type = '{$entry['type']}' and enddate < CURDATE()");
+      $db->delete_query("reservationsmodread", "entry_id NOT IN (SELECT entry_id FROM " . TABLE_PREFIX . "reservationsentry)");
     }
   }
-      add_task_log($task, "Reservierungen bereinigt");
+  add_task_log($task, "Reservierungen bereinigt");
 }
